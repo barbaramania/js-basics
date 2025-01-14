af2c37c8d32a5e82f6bc76fd20d1ba9d9a15177d
Varvara Maslouskaya <super-varyaya@ya.ru>
2025-01-14 11:15:34 -0800

    adds assign5

---
+++ assign5.js
@@ -0,0 +1,14 @@
+let firstExp = 5 + 7;
+console.log(firstExp);
+
+let secondExp = 67 - 8;
+console.log(secondExp);
+
+let thirdExp = 67 * 8;
+console.log(thirdExp);
+
+let forthExp = 67 / 8;
+console.log(forthExp);
+
+let fifthExp = 67 % 8;
+console.log(fifthExp);
