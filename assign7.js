f4c747e77b68fa25e1bff89ae7c3579c4a9c880d
Varvara Maslouskaya <super-varyaya@ya.ru>
2025-01-16 11:28:37 -0800

    adds assign7

---
+++ assign7.js
@@ -0,0 +1,95 @@
+x = 2
+
+if (x == 2) {
+    console.log(true)
+}
+if (x === 2) {
+    console.log(true)
+}
+if (x != 3) {
+    console.log(true)
+}
+if (x !== 3) {
+    console.log(true)
+}
+if (x > 1) {
+    console.log(true)
+}
+if (x < 3) {
+    console.log(true)
+}
+if (x >= 2) {
+    console.log(true)
+}
+if (x <= 2) {
+    console.log(true)
+}
+if (x < 3 && x > 1) {
+    console.log(true)
+}
+if (x < 3 || x > 2) {
+    console.log(true)
+}
+x = false
+if (!x) {
+    console.log(true)
+}
+
+// false:
+x = 3
+if (x == 2) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+if (x === 2) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+if (x != 3) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+if (x !== 3) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+if (x > 4) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+if (x < 3) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+if (x >= 4) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+if (x <= 2) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+if (x < 3 && x > 2) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+if (x < 3 || x > 4) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+x = true
+if (!x) {
+    console.log(true)
+} else {
+    console.log(false)
+}
